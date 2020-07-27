ee251120ba150cfd71b84c77e7b99cc207f33ef1
zhangyanfei1 <283929908@qq.com>
2020-07-27 23:07:56 +0800

    提代码

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,72 +1,22 @@
 <template>
-  <div id="home">
-    <nav-bar class="home-nav">
-      <div slot="center">购物街</div>
-    </nav-bar>
-    <home-swiper :banners="banners"/>
-    <recommend-view :recommends="recommends"/>
-    <feature-view></feature-view>
-    <tab-controll @tabClick="tabClick" class="tab-control" :titles="['流行', '新款', '精选']"></tab-controll>
-    <div>123</div>
-    <div>123</div>
-    <div>123</div>
-    <div>123</div>
-    <div>123</div>
-    <div>123</div>
-    <div>123</div>
-    <div>123</div>
-    <div>123</div>
-    <div>123</div>
-    <div>123</div>
-    <div>123</div>
-    <div>123</div>
-    <div>123</div>
-    <div>123</div>
-    <div>123</div>
-    <div>123</div>
-    <div>123</div>
-    <div>123</div>
-    <div>123</div>
-    <div>123</div>
-    <div>123</div>
-    <div>123</div>
-    <div>123</div>
-    <div>123</div>
-    <div>123</div>
-    <div>123</div>
-    <div>123</div>
-    <div>123</div>
-    <div>123</div>
-    <div>123</div>
-    <div>123</div>
-    <div>123</div>
-    <div>123</div>
-    <div>123</div>
-    <div>123</div>
-    <div>123</div>
-    <div>123</div>
-    <div>123</div>
-    <div>123</div>
-    <div>123</div>
-    <div>123</div>
-    <div>123</div>
-    <div>123</div>
-    <div>123</div>
-    <div>123</div>
-    <div>123</div>
-    <div>123</div>
-    <div>123</div>
-    <div>123</div>
-    <div>123</div>
-    <div>123</div>
-    <div>123</div>
-    <div>123</div>
-    <div>123</div>
-    <div>123</div>
-    <div>123</div>
-    <div>123</div>
-    <div>123</div>
-    <div>123</div>
+  <div id="home" class="wrapper">
+    <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
+    <scroll class="content"
+            ref="scroll"
+            :probe-type="3"
+            @scroll="contentScroll"
+            :pull-up-load="true"
+            @pullingUp="loadMore">
+      <home-swiper :banners="banners"/>
+      <recommend-view :recommends="recommends"/>
+      <feature-view/>
+      <tab-control class="tab-control"
+                   :titles="['流行', '新款', '精选']"
+                   @tabClick="tabClick"/>
+      <good-list :goods="showGoods"/>
+    </scroll>
+    <div>呵呵呵呵</div>
+    <back-top @click.native="backClick" v-show="isShowBackTop"/>
   </div>
 </template>
 
@@ -76,37 +26,53 @@
   import HomeSwiper from './childComps/HomeSwiper'
   import RecommendView from './childComps/RecommendView'
   import FeatureView from './childComps/FeatureView'
-  import TabControll from '../../components/content/tabControl/TabControll'
+  import TabControl from '../../components/content/tabControl/TabControll'
+  import GoodList from '../../components/content/goods/GoodsList'
+  import Scroll from '../../components/common/scroll/Scroll'
+  import BackTop from '../../components/content/backTop/BackTop'
   export default {
     name: "Home",
     components: {
-      NavBar,
       HomeSwiper,
       RecommendView,
       FeatureView,
-      TabControll
+      NavBar,
+      TabControl,
+      GoodList,
+      Scroll,
+      BackTop
     },
-    data () {
+    data() {
       return {
         banners: [],
         recommends: [],
         goods: {
           'pop': {page: 0, list: []},
           'new': {page: 0, list: []},
-          'sell': {page: 0, list: []}
-        }
+          'sell': {page: 0, list: []},
+        },
+        currentType: 'pop',
+        isShowBackTop: false
       }
     },
-    created () {
-      //1.请求多个数据
+    computed: {
+      showGoods() {
+        return this.goods[this.currentType].list
+      }
+    },
+    created() {
+      // 1.请求多个数据
       this.getHomeMultidata()
+
       // 2.请求商品数据
-      
       this.getHomeGoods('pop')
       this.getHomeGoods('new')
       this.getHomeGoods('sell')
     },
     methods: {
+      /**
+       * 事件监听相关的方法
+       */
       tabClick(index) {
         switch (index) {
           case 0:
@@ -120,32 +86,47 @@
             break
         }
       },
-      
-        getHomeMultidata () {
-          getHomeMultidata().then(res => {
-            this.banners = res.data.banner.list;
-            this.recommends = res.data.recommend.list;
-          })
-        },
-        getHomeGoods (type) {
-          const page = this.goods[type].page + 1
-          getHomeGoods(type, page).then(res => {
-            console.log(res)
-            this.goods[type].list.push(...res.data.list)
-            this.goods[type].page += 1
-          })
-        }
+      backClick() {
+        this.$refs.scroll.scrollTo(0, 0)
+      },
+      contentScroll(position) {
+        this.isShowBackTop = (-position.y) > 1000
+      },
+      loadMore() {
+        this.getHomeGoods(this.currentType)
+      },
+      /**
+       * 网络请求相关的方法
+       */
+      getHomeMultidata() {
+        getHomeMultidata().then(res => {
+          // this.result = res;
+          this.banners = res.data.banner.list;
+          this.recommends = res.data.recommend.list;
+        })
+      },
+      getHomeGoods(type) {
+        const page = this.goods[type].page + 1
+        getHomeGoods(type, page).then(res => {
+          this.goods[type].list.push(...res.data.list)
+          this.goods[type].page += 1
+
+          this.$refs.scroll.finishPullUp()
+        })
       }
-    
+    }
   }
 </script>
 
 <style scoped>
-#home {
-  padding-top: 44px;
-}
-.home-nav {
-  background-color: var(--color-tint);
+  #home {
+    /*padding-top: 44px;*/
+    height: 100vh;
+    position: relative;
+  }
+
+  .home-nav {
+    background-color: var(--color-tint);
     color: #fff;
 
     position: fixed;
@@ -153,10 +134,27 @@
     right: 0;
     top: 0;
     z-index: 9;
-}
-.tab-control {
+  }
+
+  .tab-control {
     position: sticky;
     top: 44px;
     z-index: 9;
   }
+
+  .content {
+    overflow: hidden;
+
+    position: absolute;
+    top: 44px;
+    bottom: 49px;
+    left: 0;
+    right: 0;
+  }
+
+  /*.content {*/
+    /*height: calc(100% - 93px);*/
+    /*overflow: hidden;*/
+    /*margin-top: 44px;*/
+  /*}*/
 </style>
